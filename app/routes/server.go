d48e0a9df2fc50612c75ca7ded625049fc0bae61
Travis Avey <travis.avey@pm.me>
2024-02-16 11:38:55 -0500

    templating added with a rudimentary layout/structure

--- app/routes/server.go
+++ app/routes/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 
 	"github.com/gorilla/mux"
 )
@@ -16,8 +17,32 @@ func Init() {
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(r.Proto)
 
-		w.Write([]byte("<h1>Hello World</h1>"))
+		data := struct {
+			Text string
+		}{
+			Text: "<h1>Hello, Templates</h1>",
+		}
+
+		files := getBaseTemplates()
+		files = append(files, "web/templates/index.html")
+
+		t, _ := template.ParseFiles(files...)
+		err := t.ExecuteTemplate(w, "base", data)
+		if err != nil {
+			fmt.Println("Error executing template: ", err.Error())
+		}
 	})
 
 	log.Fatal(http.ListenAndServe(":9000", router))
+}
+
+// TODO: move this method to another file in the future
+// maybe a utils file, or another if there is
+// functionality needed for the go templating
+func getBaseTemplates() []string {
+	return []string{
+		"web/templates/base.html",
+		"web/templates/partials/nav.html",
+		"web/templates/partials/footer.html",
+	}
 }
