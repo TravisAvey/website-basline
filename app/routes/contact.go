19b3568afa338a44d347a4767144efde33ca63bc
Travis Avey <travis.avey@pm.me>
2024-02-19 12:18:44 -0500

    very basic routing
    Will change for htmx
    
    Will need more routes for blog posts, gallery pics, dashboard, etc

---
+++ app/routes/contact.go
@@ -0,0 +1,24 @@
+package routes
+
+import (
+	"fmt"
+	"net/http"
+	"text/template"
+)
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	data := struct {
+		Text string
+	}{
+		Text: "<h1>Contact</h1>",
+	}
+
+	files := getBaseTemplates()
+	files = append(files, "web/templates/index.html")
+	t, _ := template.ParseFiles(files...)
+	err := t.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		// TODO: log error
+		fmt.Println("Error executing template: ", err.Error())
+	}
+}
---
+++ app/routes/util.go
@@ -0,0 +1,15 @@
+package routes
+
+func getBaseTemplates() []string {
+	return []string{
+		"web/templates/base.html",
+		"web/templates/partials/header.html",
+		"web/templates/partials/nav.html",
+		"web/templates/partials/footer.html",
+	}
+}
+
+// TODO: create a routine for dashboard templates
+// similar to getBaseTemplates
+
+// TODO: create a routine for blog templates
