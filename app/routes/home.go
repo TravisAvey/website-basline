6a8c87e5fcec2afcbcc10afdce818239dcf55aa8
Travis Avey <travis.avey@pm.me>
2024-02-19 16:10:47 -0500

    removed unused files and commented out code

--- app/routes/home.go
+++ app/routes/home.go
@@ -7,24 +7,6 @@ import (
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	/*
-		  *
-		  *
-			data := struct {
-				Text string
-			}{
-				Text: "<h1>Hello</h1>",
-			}
-
-			files := getBaseTemplates()
-			files = append(files, "web/templates/index.html")
-			t, _ := template.ParseFiles(files...)
-			err := t.ExecuteTemplate(w, "base", data)
-			if err != nil {
-				// TODO: log error
-				fmt.Println("Error executing template: ", err.Error())
-			}
-	*/
 	page := views.Page("Hello, index!")
 	page.Render(r.Context(), w)
 }
