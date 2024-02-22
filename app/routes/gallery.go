55907a605f3b127581aace98a1841f2627c4df0f
Travis Avey <travis.avey@pm.me>
2024-02-22 18:35:50 -0500

    Working on image endpoints

--- app/routes/gallery.go
+++ app/routes/gallery.go
@@ -1,8 +1,13 @@
 package routes
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/travisavey/baseline/app/database"
 )
 
 func gallery(w http.ResponseWriter, _ *http.Request) {
@@ -20,10 +25,45 @@ func gallery(w http.ResponseWriter, _ *http.Request) {
 }
 
 // upload/create a image
+func newImage(w http.ResponseWriter, r *http.Request) {
+	image, err := parseImageData(r)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+	}
+
+	err = database.CreateImage(image)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+	}
+}
 
 // get a single image
+func getImage(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.ParseUint(mux.Vars(r)["id"], 10, 64)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	image, err := database.GetImage(id)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	img := fmt.Sprint(image)
+	w.Write([]byte(img))
+}
 
 // get all images
+func getImages(w http.ResponseWriter, r *http.Request) {
+	images, err := database.GetAllImages()
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	img := fmt.Sprint(images)
+	w.Write([]byte(img))
+}
 
 // update a image
 
--- app/routes/utils.go
+++ app/routes/utils.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/travisavey/baseline/app/database"
 )
@@ -21,6 +22,10 @@ func parseFormData(r *http.Request) (database.Post, error) {
 		return database.Post{}, err
 	}
 
+	catID, err := strconv.ParseInt(r.FormValue("categoryId"), 10, 64)
+	if err != nil {
+		return database.Post{}, err
+	}
 	post := database.Post{
 		Article: database.Article{
 			Title:    r.FormValue("title"),
@@ -36,11 +41,42 @@ func parseFormData(r *http.Request) (database.Post, error) {
 		// also, figure out how to parse an array here
 		Categories: []database.Category{
 			{
-				ID:       1,
-				Category: r.FormValue("category"),
+				ID: catID,
 			},
 		},
 	}
 
 	return post, nil
+}
+
+func parseImageData(r *http.Request) (database.Image, error) {
+	err := r.ParseForm()
+	if err != nil {
+		return database.Image{}, err
+	}
+
+	isGallery, err := strconv.ParseBool(r.FormValue("isGallery"))
+	if err != nil {
+		return database.Image{}, err
+	}
+	catID, err := strconv.ParseUint(r.FormValue("categoryId"), 10, 64)
+	if err != nil {
+		return database.Image{}, err
+	}
+
+	image := database.Image{
+		Image: database.Photo{
+			ImageURL:  r.FormValue("imageURL"),
+			Title:     r.FormValue("title"),
+			Summary:   r.FormValue("summary"),
+			IsGallery: isGallery,
+		},
+		Categories: []database.ImageCategory{
+			{
+				ID: catID,
+			},
+		},
+	}
+
+	return image, nil
 }
