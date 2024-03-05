7757702646fa4ab65dac5fae6ffc5cf450493665
Travis Avey <travis.avey@pm.me>
2024-03-05 11:10:24 -0500

    Added contact from
    backend receives and just prints to console, also need to handle request and inform user

--- app/routes/contact.go
+++ app/routes/contact.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
@@ -20,6 +21,16 @@ func contact(w http.ResponseWriter, _ *http.Request) {
 }
 
 // endpoint for a post request for a contact here
+func contactForm(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	fmt.Println(r.FormValue("name"), r.FormValue("email"), r.FormValue("message"))
+}
+
 //
 // TODO: think how this will work: fire off an email
 // what email service to setup and use in the backend here?
