657a2250d5b80c5d96decc57b2563b75910a74ca
Travis Avey <travis.avey@pm.me>
2024-03-29 09:22:05 -0400

    Showing / hiding delete dialog works

--- web/static/src/js/script.js
+++ web/static/src/js/script.js
@@ -3,3 +3,8 @@ function showDeleteDialog(id) {
   ele = "modal_" + id
   document.getElementById(ele).showModal()
 }
+
+function closeDeleteDialog(id) {
+  ele = "modal_" + id
+  document.getElementById(ele).close()
+}
