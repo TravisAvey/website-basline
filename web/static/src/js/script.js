153c5fc6768d47e9677f4d711f909795a118cf07
Travis Avey <travis.avey@pm.me>
2024-04-21 06:08:16 -0400

    getting post removed from dashboard view once deleted
    added const to other js methods

--- web/static/src/js/script.js
+++ web/static/src/js/script.js
@@ -1,10 +1,15 @@
 
 function showDeleteDialog(id) {
-  ele = "modal_" + id
+  const ele = "modal_" + id
   document.getElementById(ele).showModal()
 }
 
 function closeDeleteDialog(id) {
-  ele = "modal_" + id
+  const ele = "modal_" + id
   document.getElementById(ele).close()
+}
+
+function removeElement(id) {
+  const ele = "post-" + id
+  document.getElementById(ele).remove()
 }
