e4390af06de33282bc979815f0f72daf4f368af4
Travis Avey <travis.avey@pm.me>
2024-04-28 05:26:39 -0400

    message system is working

--- app/routes/sse.go
+++ app/routes/sse.go
@@ -1,28 +1,25 @@
 package routes
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 var msgChan chan string
 
 func sseEndpoint(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("sseEndpoint... setting up")
 	// get some id
-	//
 
-	// set up sse
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	msgChan = make(chan string)
-	fmt.Println("sseEndpoint: msgChan = make(chan string)")
 
-	// Create a context for handling client disconnection
 	_, cancel := context.WithCancel(r.Context())
 	defer cancel()
 
@@ -52,9 +49,12 @@ func sseEndpoint(w http.ResponseWriter, r *http.Request) {
 func formatSSEMessage(msg string) string {
 	var message string
 	message = "event: message\n"
-	// might need a loop here... and do data: %s\n for each line
-	// then a single extra \n at the end
-	message += fmt.Sprintf("data: %s\n\n", msg)
+
+	scanner := bufio.NewScanner(strings.NewReader(msg))
+	for scanner.Scan() {
+		message += fmt.Sprintf("data: %s\n", scanner.Text())
+	}
+	message += "\n"
 
 	return message
 }
