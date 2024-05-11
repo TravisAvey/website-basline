9c572cd3bdf543f6325bbb8065c0d782098027d0
Travis Avey <travis.avey@pm.me>
2024-05-11 16:36:50 -0400

    Added in tiptap
    looks to function OK

--- web/static/src/js/editor.js
+++ web/static/src/js/editor.js
@@ -1,10 +1,22 @@
 import { Editor } from '@tiptap/core'
-import StarterKit from '@tiptap/starter-kit'
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+import Heading from '@tiptap/extension-heading'
 
 new Editor({
-  element: document.querySelector('.editor'),
+  element: document.querySelector('#editor'),
   extensions: [
-    StarterKit,
+    Document,
+    Paragraph,
+    Text,
+    Heading.configure({
+      levels: [1,2,3],
+    })
   ],
-  content: '<p>Hello World!</p>',
+  content: '<p>Example Text</p>',
+  autofocus: true,
+  editable: true,
+  injectCSS: false,
 })
+
