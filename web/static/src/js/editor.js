102bd6dbbe4b83ae11c07089ef61614aab465e59
Travis Avey <travis.avey@pm.me>
2024-05-12 19:09:16 -0400

    updated h1-h3 icons--font awesome's icons for these are pro

--- web/static/src/js/editor.js
+++ web/static/src/js/editor.js
@@ -1,5 +1,6 @@
 import { Editor } from '@tiptap/core'
 import Document from '@tiptap/extension-document'
+import History from '@tiptap/extension-history'
 import Paragraph from '@tiptap/extension-paragraph'
 import Text from '@tiptap/extension-text'
 import Heading from '@tiptap/extension-heading'
@@ -25,6 +26,7 @@ class EditorController {
       element: this.editorElement,
       extensions: [
         Document,
+        History,
         Paragraph,
         Text,
         Bold.configure({
