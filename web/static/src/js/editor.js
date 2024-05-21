e75199cd009995b43fa68ac849d6ec0bbff617a4
Travis Avey <travis.avey@pm.me>
2024-05-20 21:13:01 -0400

    categories add/remove for a post is sorted out and is sent to backend in a comma separated list
    may need a ticket for creating a new category on the post creation and post edit page
    
    next is to handle storing this in the db
    
    another ticket for figuring out how to make sure only posts with post-date that are date and prior are shown in blog

--- web/static/src/js/editor.js
+++ web/static/src/js/editor.js
@@ -233,3 +233,48 @@ class EditorController {
 
 const editorController = new EditorController("editor", "Write Your Epic Content")
 
+// our container of all our categories buttons
+const cats = document.getElementById("categories")
+// our hidden input to put our values for hx-post to server
+var catOutput = document.getElementById("categories-output")
+// a list to keep track of all our categories
+var categories = []
+
+for (const cat of cats.children) {
+  cat.addEventListener("click", event => {
+
+    // get current category and index
+    const current = cat.innerText
+    const index = categories.indexOf(current)
+
+    // if the class is badge-outline, then it's not
+    // a selected category
+    if (cat.classList.contains("badge-outline")) {
+      // flip the look of selected
+      cat.classList.remove("badge-outline")
+      cat.classList.add("badge-success")
+
+      // if it's not already in list
+      // add the category to list
+      if (index == -1) {
+       categories.push(current) 
+      }
+
+      // else its already a selected category
+      // so flip the look..
+    } else {
+      cat.classList.remove("badge-success")
+      cat.classList.add("badge-outline")
+
+      // check if in (should be)
+      if (index >= 0) {
+        // remove the category
+        categories.splice(index, 1)
+      }
+    }
+    // regardless, update the list of categories
+    // and put in our hidden value input
+    catOutput.value = categories
+  })
+}
+
